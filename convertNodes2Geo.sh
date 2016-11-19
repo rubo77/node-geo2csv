9e1fe1c2cb460dbc28f487840f0204f7e9870184
rubo77 <github@r.z11.de>
2016-11-19 04:05:00 +0100

    directly convert to csv file

--- convertNodes2Geo.sh
+++ convertNodes2Geo.sh
@@ -1,3 +1,5 @@
 wget https://hopglass.freifunk.in-kiel.de/nodes.json -O nodes.json
-jq .nodes[].nodeinfo.location <nodes.json>geolist.json
+jq '[.nodes[] |select(.nodeinfo.location)| .nodeinfo.hostname,.nodeinfo.location]' <nodes.json > geolist.json
 echo geolist.json generated
+jq '.nodes[] |select(.nodeinfo.location)|[ .nodeinfo.hostname,(.nodeinfo.location.longitude | tostring),(.nodeinfo.location.latitude | tostring),(.nodeinfo.location.altitude | tostring)] | join("; ")' <nodes.json|sed 's/"//g' > geolist.csv
+echo geolist.csv generated
