8a088c960c6dea942a3b170ce56c32f9ca0d07ab
rubo77 <github@r.z11.de>
2016-11-19 04:15:50 +0100

    url as argument

--- convertNodes2Geo.sh
+++ convertNodes2Geo.sh
@@ -1,4 +1,14 @@
-wget https://hopglass.freifunk.in-kiel.de/nodes.json -O nodes.json
+#!/bin/bash
+if [ "$1" == "--help" ] || [ "$1" == "" ]; then
+        echo usage: $0 nodes.jsonurl
+	echo for example 
+	echo $0 https://hopglass.freifunk.in-kiel.de/nodes.json
+	echo 
+	echo '!!! Must be called from within the script folder !!!'
+	echo
+	exit 0
+fi
+wget "$1" -O nodes.json
 jq '[.nodes[] |select(.nodeinfo.location)| .nodeinfo.hostname,.nodeinfo.location]' <nodes.json > geolist.json
 echo geolist.json generated
 jq '.nodes[] |select(.nodeinfo.location)|[ .nodeinfo.hostname,(.nodeinfo.location.longitude | tostring),(.nodeinfo.location.latitude | tostring),(.nodeinfo.location.altitude | tostring)] | join("; ")' <nodes.json|sed 's/"//g' > geolist.csv
